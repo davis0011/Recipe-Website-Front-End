83de399e036ad9b492d4fcb381ab0fb2375c1d40
davis0011 <Davis85754556>
2023-06-15 15:44:26 +0300

    this now works

---
+++ src/components/RandomRecipes.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <randomRecipes></randomRecipes>
+    </div>
+</template>
+
+<script>
+import RecipePreview from "./RecipePreview.vue";
+export default{
+    name: "RandomRecipes",
+    components:{
+        // RecipePreview,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
